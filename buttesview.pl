b73976bdadc9b014adf32d334c0b28b8ca8ee909
Lee <leedo@zaphod.servercentral.net>
2009-07-23 13:03:18 -0500

    Fix buttesviewer

--- buttesview.pl
+++ buttesview.pl
@@ -7,7 +7,7 @@ use Gtk2 -init;
 use Gtk2::WebKit;
 
 my $window = Gtk2::Window->new;
-my $view = Gtk2::WebView->new;
+my $view = Gtk2::WebKit::WebView->new;
 $window->add($view);
 $view->open("http://127.0.0.1:8080/view");
 $window->show_all;
