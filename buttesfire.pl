5bdcd85d1d6d27855758a540891b0e19012f7613
Lee Aylward <lee@laylward.com>
2009-08-01 11:47:16 -0500

    Fallback to port 8080, fix missing page title

--- buttesfire.pl
+++ buttesfire.pl
@@ -9,14 +9,16 @@ use local::lib 'extlib';
 use YAML qw/LoadFile/;
 use Buttes::HTTPD;
 use Buttes::IRC;
+use Sys::Hostname;
 use POE;
 
 $0 = 'buttesfire-web';
 my $config = LoadFile($ENV{HOME}.'/.buttesfire.yaml');
+$config->{port} ||= 8080;
   
 BEGIN { $SIG{__WARN__} = sub { warn $_[0] if $config->{debug} } };
 
-print STDERR "You can view your IRC session at: http://localhost:8080/view";
+print STDERR "You can view your IRC session at: http://".hostname.":".$config->{port}."/view";
 
 my $httpd = Buttes::HTTPD->new(config => $config);
 my $irc = Buttes::IRC->new(config => $config, httpd => $httpd);
