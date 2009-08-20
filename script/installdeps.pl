dd7bbe93ff03761ef0cf1f950a38e24d30da6095
Lee Aylward <lee@laylward.com>
2009-08-20 17:04:01 -0500

    Make installdeps smarter

--- script/installdeps.pl
+++ script/installdeps.pl
@@ -3,19 +3,38 @@
 use CPANPLUS::Backend;
 
 my $cb = CPANPLUS::Backend->new;
+$cb->flush('all');
 my $conf = $cb->configure_object;
 
 $conf->set_conf(prereqs => 1);
 $conf->set_conf(verbose => 1);
 $conf->set_conf(signature => 0);
-$conf->set_conf(force => 1);
+$conf->set_conf(force => 0);
 $conf->set_conf(skiptest => 1);
 $conf->set_conf(allow_build_interactivity => 0);
 
-$cb->install(modules => [
-  qw/POE POE::Component::IRC POE::Component::Server::HTTP
-  POE::Component::SSLify Moose MooseX::POE
-  MooseX::ClassAttribute MooseX::Declare YAML
-  JSON Template Template::Plugin::JavaScript
-  IRC::Formatting::HTML DateTime File::ShareDir
-  Digest::CRC/]);
+my @modules = qw/
+  ExtUtils::Depends
+  YAML
+  Class::MOP
+  Moose 
+  MooseX::ClassAttribute
+  MooseX::Declare
+  POE
+  POE::Component::IRC
+  POE::Component::Server::HTTP
+  POE::Component::SSLify
+  MooseX::POE
+  JSON
+  Template
+  Template::Plugin::JavaScript
+  IRC::Formatting::HTML
+  DateTime
+  File::ShareDir
+  Digest::CRC
+/;
+
+for (@modules) {
+  $cb->install(modules => [$_]);
+}
+
