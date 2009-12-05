fada2505393dadcf5c28fe77f6cd0330344657eb
Lee Aylward <lee@laylward.com>
2009-12-05 01:57:34 -0600

    Re-added HTTP auth

--- lib/App/Alice/HTTPD.pm
+++ lib/App/Alice/HTTPD.pm
@@ -64,6 +64,7 @@ sub BUILD {
     '/logs'         => sub{$self->send_logs(@_)},
     '/search'       => sub{$self->send_search(@_)},
     'client_disconnected' => sub{$self->purge_disconnects(@_)},
+    request         => sub{$self->check_authentication(@_)},
   );
   $self->httpd($httpd);
   $self->ping;
@@ -102,12 +103,10 @@ sub broadcast {
 
 sub check_authentication {
   my ($self, $httpd, $req) = @_;
-  unless ($self->config->auth
+  return unless ($self->config->auth
       and ref $self->config->auth eq 'HASH'
       and $self->config->auth->{username}
-      and $self->config->auth->{password}) {
-    $req->respond([200,'ok']) 
-  }
+      and $self->config->auth->{password});
 
   if (my $auth  = $req->headers->{authorization}) {
     $auth =~ s/^Basic //;
@@ -115,13 +114,13 @@ sub check_authentication {
     my ($user,$password)  = split(/:/, $auth);
     if ($self->config->auth->{username} eq $user &&
         $self->config->auth->{password} eq $password) {
-      $req->respond([200,'ok']);
       return;
     }
     else {
       $self->log_debug("auth failed");
     }
   }
+  $httpd->stop_request;
   $req->respond([401, 'unauthorized', {'WWW-Authenticate' => 'Basic realm="Alice"'}]);
 }
 
--- share/static/alice.js
+++ share/static/alice.js
@@ -7442,7 +7442,6 @@ Alice.Connection = Class.create({
       var queue = data.queue;
       var length = queue.length;
       for (var i=0; i<length; i++) {
-        console.log(queue[i]);
         if (queue[i].type == "action")
           this.application.handleAction(queue[i]);
         else if (queue[i].type == "message") {
