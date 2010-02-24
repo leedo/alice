5b990ab3b5868c24dfcbac4d498fde56818b539d
Lee Aylward <lee@laylward.com>
2010-02-23 19:31:29 -0600

    Clean up InfoWindow

--- lib/App/Alice/InfoWindow.pm
+++ lib/App/Alice/InfoWindow.pm
@@ -10,20 +10,22 @@ extends 'App::Alice::Window';
 has '+is_channel' => (lazy => 0, default => 0);
 has '+id' => (default => 'info');
 has '+title' => (required => 0, default => 'info');
-has '+irc' => (required => 0, lazy => 1, default => sub {
-  my $self = shift;
-  if ($self->app->connected_ircs == 1) {
-    return ($self->app->connected_ircs)[0];
-  }
-  $self->app->broadcast(
-    $self->format_announcement("Connected to multiple servers and no server specified! /command -server args")
-  );
-  return undef;
-});
 has '+session' => ( isa => 'Undef', default => undef);
 has 'topic' => (is => 'ro', isa => 'HashRef', default => sub {{string => ''}});
 has '+buffersize' => (default => 300);
 has '+type' => (lazy => 0, default => 'info');
+
+has '+irc' => (
+  required => 0,
+  lazy     => 1,
+  default  => sub {
+    my $self = shift;
+    return ($self->app->connected_ircs)[0] if $self->app->connected_ircs == 1;
+    $self->app->broadcast(
+      $self->format_announcement("No server specified! /command -server args"));
+    return undef;
+  }
+);
 
 sub format_message {
   my ($self, $from, $body, $highlight, $monospaced) = @_;
