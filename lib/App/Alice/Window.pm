cb89b06ce03d73c9b046d28063be8936167049cd
leedo <lee@laylward.com>
2010-07-27 16:27:39 -0500

    use gruber's new url regex

--- lib/App/Alice/Window.pm
+++ lib/App/Alice/Window.pm
@@ -256,7 +256,7 @@ sub nick_table {
 
 sub make_links_clickable {
   my $html = shift;
-  $html =~ s/\b(([\w-]+:\/\/?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/)))/<a href="$1" target="_blank" rel="noreferrer">$1<\/a>/gi;
+  $html =~ s/(?i)\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/<a href="$1" target="_blank" rel="noreferrer">$1<\/a>/gi;
   return $html;
 }
 
