ef91df60f189fb478473e6c79fc42a96347ae333
leedo <lee@laylward.com>
2010-08-07 10:31:31 -0500

    Fix tab order saving
    this broke because i removed the "w" prefix on window ids

--- src/js/alice/src/alice/application.js
+++ src/js/alice/src/alice/application.js
@@ -284,7 +284,7 @@ Alice.Application = Class.create({
       onUpdate: function (res) {
         var tabs = res.childElements();
         var order = tabs.collect(function(t){
-          var m = t.id.match(/(w[^_]+)_tab/);
+          var m = t.id.match(/([^_]+)_tab/);
           if (m) return m[1]
         });
         if (order.length) this.connection.sendTabOrder(order);
