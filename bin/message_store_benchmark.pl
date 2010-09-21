f8dc66d0120b849ce5c733c14963e8edb300469b
leedo <lee@laylward.com>
2010-09-21 11:28:28 -0500

    added script to benchmark message stores

---
+++ bin/message_store_benchmark.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib';
+use App::Alice::MessageBuffer;
+use Benchmark qw/:all/;
+use AnyEvent;
+
+my $id = time;
+my $data = { 
+  event => "say",
+  nick => $ENV{USER},
+  html => join "\n", map {$_ => "$_" x 300} (0 .. 10)
+};
+
+my %stores = map {$_ => App::Alice::MessageBuffer->new(id => $id, store_class => $_)} qw/Memory TokyoCabinet Cache/;
+my $cv = AE::cv;
+my $t = AE::timer 1, 0, sub {
+  print STDERR "timing\n";
+  cmpthese(
+    5000, {
+      map {
+        my $store = $stores{$_};
+        $_ => sub {
+          $store->add($data) for 0 .. 100;
+        }
+      } keys %stores
+    }
+  );
+  $cv->send;
+};
+$cv->wait;
