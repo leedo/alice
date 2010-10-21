80f002dc88126821b4389bc93276a61272f15d4c
leedo <lee@laylward.com>
2010-10-21 16:13:40 -0500

    use Plack::Middleware::WebSocket

--- lib/App/Alice/Stream/WebSocket.pm
+++ lib/App/Alice/Stream/WebSocket.pm
@@ -7,7 +7,7 @@ use Time::HiRes qw/time/;
 
 extends 'App::Alice::Stream';
 
-has env => (
+has fh => (
   is => 'ro',
   required => 1,
 );
@@ -29,38 +29,7 @@ has is_xhr => (
 sub BUILD {
   my $self = shift;
 
-  my $env = $self->env;
-
-  my $hh = join "\015\012", (
-    'HTTP/1.1 101 Web Socket Protocol Handshake',
-    'Upgrade: WebSocket',
-    'Connection: Upgrade',
-    "Sec-WebSocket-Origin: $env->{HTTP_ORIGIN}",
-    "Sec-WebSocket-Location: ws://$env->{HTTP_HOST}$env->{SCRIPT_NAME}$env->{PATH_INFO}?$env->{QUERY_STRING}",
-    '',
-  );
-
-  my $fh = $env->{'psgix.io'};
-
-  my $key1 = $env->{'HTTP_SEC_WEBSOCKET_KEY1'};
-  my $key2 = $env->{'HTTP_SEC_WEBSOCKET_KEY2'};
-  my $n1 = join '', $key1 =~ /\d+/g;
-  my $n2 = join '', $key2 =~ /\d+/g;
-  my $s1 = $key1 =~ y/ / /;
-  my $s2 = $key2 =~ y/ / /;
-  $n1 = int($n1 / $s1);
-  $n2 = int($n2 / $s2);
-
-  my $len = read $fh, my $chunk, 8;
-  die "read: $!" unless defined $len;
-
-  my $string = pack('N', $n1) . pack('N', $n2) . $chunk;
-  my $digest = md5 $string;
-  
-  my $h = AnyEvent::Handle->new(fh => $fh);
-  $h->push_write($hh);
-  $h->push_write("\015\012");
-  $h->push_write("$digest");
+  my $h = AnyEvent::Handle->new(fh => $self->fh);
   
   $h->on_error(sub {
     warn $_[2];
@@ -75,12 +44,8 @@ sub BUILD {
 
   $h->on_read(sub {
     $_[0]->push_read(
-      line => "\xff",
-      sub {
-        my ($h, $line) = @_;
-        $line =~ s/^\0// or warn;
-        $self->on_read->(decode_json $line);
-      }
+      'AnyEvent::Handle::Message::WebSocket',
+      sub { $self->on_read->(decode_json $_[1]) }
     );
   });
     
@@ -95,12 +60,15 @@ sub send {
     {utf8 => 1, shrink => 1}
   );
   
-  $self->handle->push_write("\x00$line\xff");
+  $self->send_raw($line);
 }
 
 sub send_raw {
   my ($self, $string) = @_;
-  $self->handle->push_write("\x00$string\xff");
+  $self->handle->push_write(
+    'AnyEvent::Handle::Message::WebSocket',
+    $string
+  );
 }
 
 sub close {
