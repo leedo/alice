a8b840e6851f73e0517b4e8aa9e5bcaf706bb67b
leedo <lee@laylward.com>
2011-01-22 22:57:01 -0600

    simplify standalone condvar usage

--- lib/App/Alice/Standalone.pm
+++ lib/App/Alice/Standalone.pm
@@ -13,13 +13,10 @@ has cv => (
 after run => sub {
   my $self = shift;
 
+  my @sigs = map {AE::signal $_, sub {$self->init_shutdown}} qw/INT QUIT/;
 
   $self->cv(AE::cv);
-
-  my @sigs = map {AE::signal $_, sub {$self->cv->send}} qw/INT QUIT/;
-
-  my $args = $self->cv->recv;
-  $self->init_shutdown(@$args);
+  $self->cv->recv;
 };
 
 after init => sub {
@@ -27,19 +24,12 @@ after init => sub {
   print STDERR "Location: http://".$self->config->http_address.":".$self->config->http_port."/\n";
 };
 
-around init_shutdown => sub {
-  my $orig = shift;
+before init_shutdown => sub {
   my $self = shift;
-
   print STDERR ($self->connected_ircs ? "\nDisconnecting, please wait\n" : "\n");
-  $self->$orig(@_);
-
-  $self->cv(AE::cv);
-  $self->cv->recv;
-  $self->shutdown;
 };
 
-before shutdown => sub {
+after shutdown => sub {
   my $self = shift;
   $self->cv->send;
 };
