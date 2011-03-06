87824de667561866da4295937427f3bbfb7b9b1a
leedo <lee@laylward.com>
2011-03-06 14:53:42 -0600

    more liberal channel regex

--- src/js/alice/src/alice.js
+++ src/js/alice/src/alice.js
@@ -111,12 +111,12 @@ if (window == window.parent) {
     // setup default filters
 
     var regexes = {
-      url: /\b(https?:\/\/(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,
+      url: /(https?:\/\/(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/g,
       twitter: /https?:\/\/(?:www\.)?twitter\.com\/(?:#!\/)?[^\/]+\/status\/(\d+)/i,
       img: /^http[^\s]*\.(?:jpe?g|gif|png|bmp|svg)[^\/]*$/i,
       audio: /^http[^\s]*\.(?:wav|mp3|ogg|aiff|m4a)[^\/]*$/i,
       gist: /^https?:\/\/gist\.github\.com\/[0-9a-fA-F]+$/i,
-      channel: /([\b>\s])(#[\w\d]+)([\b<\s])/
+      channel: /([\b>\s])(#[^\b<\s]+)([\b<\s])/
     };
     
 
