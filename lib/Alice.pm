bddc35ab867ab49e6a49fcaacc7625c918b19edb
Lee Aylward <lee@laylward.com>
2011-11-25 17:06:32 -0600

    only cancel reconnect if there is one

--- lib/Alice.pm
+++ lib/Alice.pm
@@ -353,7 +353,7 @@ sub reload_config {
     unless (exists $self->config->servers->{$name}) {
       $self->send_info("config", "removing $name server");
       if ($irc->is_disconnected) {
-        $self->cancel_reconnect($name);
+        $self->cancel_reconnect($name) if $irc->reconnect_timer;
         $irc->cl(undef);
         $self->remove_irc($name);
       }
