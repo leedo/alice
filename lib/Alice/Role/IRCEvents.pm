357b06e14d7bc1e529dd242cdc749e42e4346d1b
bpaf <edi@mbp.(none)>
2011-11-29 20:56:47 +0100

    repeated line fix

--- lib/Alice/Role/IRCEvents.pm
+++ lib/Alice/Role/IRCEvents.pm
@@ -376,7 +376,6 @@ irc_event channel_remove => sub {
     unless ($self->is_ignore(part => $channel)) {
       my $reason = "";
 
-      my $reason = "";
       if ($msg and $msg->{command} eq "QUIT") {
         $reason = $msg->{params}[-1] || "Quit";
       }
