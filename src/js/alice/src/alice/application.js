2ffb819b181fb40d56b14b3cf90c8b78c59392be
Lee Aylward <lee@laylward.com>
2011-12-19 17:45:03 -0600

    typo

--- src/js/alice/src/alice/application.js
+++ src/js/alice/src/alice/application.js
@@ -29,7 +29,7 @@ Alice.Application = Class.create({
     this.supportsTouch = 'createTouch' in document;
 
     this.isPhone = window.navigator.userAgent.match(/(android|iphone|wosbrowser)/i) ? true : false;
-    this.isMobile = this.location.toString().match(/mobile/i) || this.isPhone || Prototype.Browser.MobileSafari;
+    this.isMobile = window.location.toString().match(/mobile/i) || this.isPhone || Prototype.Browser.MobileSafari;
     this.loadDelay = this.isMobile ? 3000 : 1000;
     if (window.navigator.standalone || window.navigator.userAgent.match(/Fluid/)) this.loadDelay = 0;
     
